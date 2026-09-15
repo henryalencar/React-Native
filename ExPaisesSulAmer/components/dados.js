a0c5c7c08ab7e56cb829432bfde34d5521686ed4
Henry Alencar <henryalencar2007@gmail.com>
2026-09-15 15:00:26 -0300

    Initial commit

---
+++ ExPaisesSulAmer/components/dados.js
@@ -0,0 +1,86 @@
+export const PAISES = [
+
+  {
+    id: '1',
+    nome: 'Peru',
+    bandeira: '🇵🇪',
+
+    cultura: 'Herança direta do Império Inca, rica em tecelagem colorida e festividades solares como o Inti Raymi.',
+
+    pontoTuristico: 'Machu Picchu (Cusco) e as Linhas de Nazca.',
+
+    comidaTipica: 'Ceviche peruano, Lomo Saltado e o drink Pisco Sour.',
+
+    imagem: require('../assets/Peru.jpg'),
+
+    imagemCultura: require('../assets/culturaPeru.jpg'),
+
+    imagemTurismo: require('../assets/turismoPeru.jpg'),
+
+    imagemComida: require('../assets/cevichePeru.jpg')
+  },
+
+
+  {
+    id: '2',
+    nome: 'México',
+    bandeira: '🇲🇽',
+
+    cultura: 'Fusão das civilizações Asteca/Maia com a colonização espanhola deixou marcas profundas na religião católica e na arquitetura. Famoso pelo "Dia dos Mortos".',
+
+    pontoTuristico: 'Chichén Itzá (Pirâmide Maia) e as praias de Cancún.',
+
+    comidaTipica: 'Tacos, Guacamole, Enchiladas e as famosas pimentas.',
+
+    imagem: require('../assets/Mexico.jpg'),
+
+    imagemCultura: require('../assets/culturaMexico.jpg'),
+
+    imagemTurismo: require('../assets/turismoMexico.jpg'),
+
+    imagemComida: require('../assets/tacosMexico.jpg')
+  },
+
+
+  {
+    id: '3',
+    nome: 'Colômbia',
+    bandeira: '🇨🇴',
+
+    cultura: 'Rica expressão musical influenciada pelo ritmo da Cumbia, Salsa e o famoso festival de flores.',
+
+    pontoTuristico: 'Cidade Muralhada de Cartagena e o Parque Tayrona.',
+
+    comidaTipica: 'Bandeja Paisa, Arepas e o mundialmente famoso Café Colombiano.',
+
+    imagem: require('../assets/Colombia.jpg'),
+
+    imagemCultura: require('../assets/culturaColombia.jpg'),
+
+    imagemTurismo: require('../assets/turismoColombia.jpg'),
+
+    imagemComida: require('../assets/paisaColombia.jpg')
+  },
+
+
+  {
+    id: '4',
+    nome: 'Bolívia',
+    bandeira: '🇧🇴',
+
+    cultura: 'Fortes raízes indígenas (Aymara e Quechua), preservando trajes tradicionais e a música andina.',
+
+    pontoTuristico: 'Salar de Uyuni (maior deserto de sal do mundo) e o Lago Titicaca.',
+
+    comidaTipica: 'Salteñas (tipo de empanada suculenta) e o prato Silpancho.',
+
+    imagem: require('../assets/Bolivia.jpg'),
+
+    imagemCultura: require('../assets/culturaBolivia.jpg'),
+
+    imagemTurismo: require('../assets/turismoBolivia.jpg'),
+
+    imagemComida: require('../assets/saltenasBolivia.jpg')
+  },
+
+];
