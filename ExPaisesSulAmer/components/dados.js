c796c0d8d488e45c755c9dc9030db2245dd95c36
Henry Alencar <henryalencar2007@gmail.com>
2026-09-17 14:49:16 -0300

    React-Native

--- ExPaisesSulAmer/components/dados.js
+++ ExPaisesSulAmer/components/dados.js
@@ -83,4 +83,125 @@ export const PAISES = [
     imagemComida: require('../assets/saltenasBolivia.jpg')
   },
 
+  {
+    id: '5',
+    nome: 'Brasil',
+    bandeira: '🇧🇷',
+
+    cultura: 'Diversidade cultural influenciada pelos povos indígenas, africanos e europeus, com destaque para o carnaval e o samba.',
+
+    pontoTuristico: 'Cristo Redentor (Rio de Janeiro) e a Floresta Amazônica.',
+
+    comidaTipica: 'Prato típico brasileiro com feijão preto, carne de porco e acompanhamentos variados.',
+
+    imagem: require('../assets/Brasil.jpg'),
+
+    imagemCultura: require('../assets/culturaBrasil.jpg'),
+
+    imagemTurismo: require('../assets/turismoBrasil.jpg'),
+
+    imagemComida: require('../assets/feijoadaBrasil.jpg')
+  },
+
+  {
+    id: '6',
+    nome: 'Argentina',
+    bandeira: '🇦🇷',
+
+    cultura: 'Rica herança cultural influenciada pelos povos indígenas, europeus e italianos, com destaque para o tango e a música folclórica.',
+
+    pontoTuristico: 'Patagonia Glaciar Perito Moreno e a cidade de Buenos Aires.',
+
+    comidaTipica: 'Prato típico argentino com carne de bovino, feijão e acompanhamentos variados.',
+
+    imagem: require('../assets/Argentina.jpg'),
+
+    imagemCultura: require('../assets/culturaArgentina.jpg'),
+
+    imagemTurismo: require('../assets/turismoArgentina.jpg'),
+
+    imagemComida: require('../assets/parrillaArgentina.jpg')
+  },
+
+  {
+    id: '7',
+    nome: 'Chile',
+    bandeira: '🇨🇱',
+
+    cultura: 'Rica herança cultural influenciada pelos povos indígenas, espanhóis e europeus, com destaque para a música folclórica e a literatura chilena.',
+
+    pontoTuristico: 'Codileira dos Andes e a cidade de Santiago.',
+
+    comidaTipica: 'pastel de choclo, empanadas e frutos do mar.',
+
+    imagem: require('../assets/Chile.jpg'),
+
+    imagemCultura: require('../assets/culturaChile.jpg'),
+
+    imagemTurismo: require('../assets/turismoChile.jpg'),
+
+    imagemComida: require('../assets/pastelChile.jpg')
+  },
+
+  {
+    id: '8',
+    nome: 'Uruguai',
+    bandeira: '🇺🇾',
+
+    cultura: 'Rica herança cultural influenciada pelos povos indígenas, espanhóis e italianos, com destaque para a música folclórica e a literatura uruguaia.',
+
+    pontoTuristico: 'Cidade de Montevidéu e a região vinícola de Canelones.',
+
+    comidaTipica: 'sandwich de chivito acompanhado de batatas fritas e salada.',
+
+    imagem: require('../assets/Uruguai.jpg'),
+
+    imagemCultura: require('../assets/culturaUruguai.jpg'),
+
+    imagemTurismo: require('../assets/turismoUruguai.jpg'),
+
+    imagemComida: require('../assets/lancheUruguai.jpg')
+  },
+
+  {
+    id: '9',
+    nome: 'Paraguai',
+    bandeira: '🇵🇾',
+
+    cultura: 'Rica herança cultural influenciada pelos povos indígenas, espanhóis e italianos, com destaque para a música folclórica e a literatura paraguaia.',
+
+    pontoTuristico: 'Cidade de Assunção e as missões jesuíticas de Encarnación.',
+
+    comidaTipica: 'Sopa paraguaia, mandioca e carne de porco.',
+
+    imagem: require('../assets/Paraguai.jpg'),
+
+    imagemCultura: require('../assets/culturaParaguai.jpg'),
+
+    imagemTurismo: require('../assets/turismoParaguai.jpg'),
+
+    imagemComida: require('../assets/sopaParaguai.jpg')
+  },
+
+
+  {
+    id: '10',
+    nome: 'Equador',
+    bandeira: '🇪🇨',
+
+    cultura: 'Rica herança cultural influenciada pelos povos indígenas, espanhóis e italianos, com destaque para a música folclórica e a literatura equatoriana.',
+
+    pontoTuristico: 'Cidade de Quito e as Ilhas Galápagos.',
+
+    comidaTipica: 'Encebolado de peixe com banana verde e arroz.',
+
+    imagem: require('../assets/Equador.jpg'),
+
+    imagemCultura: require('../assets/culturaEquador.jpg'),
+
+    imagemTurismo: require('../assets/turismoEquador.jpg'),
+
+    imagemComida: require('../assets/enceboladoEquador.jpg')
+  },
+
 ];
